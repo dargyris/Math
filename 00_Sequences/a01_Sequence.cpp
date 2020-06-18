60733a3ccbdb132cd63cd27e5db081ac046e2e08
dargyris <diamantisargiris@gmail.com>
2020-06-18 17:14:23 +0200

    Optimising sequence sum calculation.

--- 00_Sequences/a01_Sequence.cpp
+++ 00_Sequences/a01_Sequence.cpp
@@ -8,29 +8,30 @@ int main(){
     system ( "clear" );
 
     int cycles {0};
-    int sum {0};
-    for ( int i {1}; i < 372; i+=5 ){
-        sum += i;
-        cycles ++;
-    }
-    cout << "Sum\t: " << sum << endl;
-    cout << "Cycles\t: " << cycles << endl;
+    int firstElement {0};
+    int step {0};
+    int numberOfElements {0};
+    int lastElement {0};
+    cout << "Please enter first Element\t> ";
+    cin >> firstElement;
+    cout << "Please enter last Element\t> ";
+    cin >> lastElement;
+    cout << "Please enter step\t\t> ";
+    cin >> step;
     cout << endl;
 
-    int firstElement {1};
-    int step {5};
-    int numberOfElements {75};
-    int lastElement {0};
-    int middleElement {0};
-    lastElement = firstElement + ( (numberOfElements - 1) * step );
-    middleElement = lastElement - (lastElement - firstElement)/2;
+    numberOfElements = (lastElement - firstElement)/step + 1;
+    cout << endl;
+
+    cout << "First\t: " << firstElement << endl;
+    cout << "Last\t: " << lastElement << endl;
+    cout << "#NoE.\t: " << numberOfElements << endl;
+    cout << endl;
+
     cycles = 0;
-    sum = 0;
-    for ( int i {0}; i < 37; ++i){
-        sum += 372;
-        cycles ++;
-    }
-    sum += middleElement;
+    int sum {0};
+    sum += numberOfElements * ( lastElement + firstElement )/2;
+    cycles ++;
     cout << "Sum\t: " << sum << endl;
     cout << "Cycles\t: " << cycles << endl;
 
